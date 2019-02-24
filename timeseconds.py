9382238de008af5317b1faee1c20f533442d63f2
Christopher <c.boyle1989@gmail.com>
2019-02-23 20:38:41 -0700

    first commit

---
+++ timeseconds.py
@@ -0,0 +1,12 @@
+#! python3
+
+print('Enter a time in seconds and the calculator will tell you that time broken into days, hours, minutes, and seconds.')
+
+timeInput = int(input('> '))
+
+seconds = str(int(((timeInput % 86400) % 3600) % 60))
+minutes = str(int(((timeInput % 86400) % 3600) / 60))
+hours = str(int((timeInput % 86400) / 3600))
+days = str(int(timeInput / 86400))
+
+print(days + ' day(s), ' + hours + ' hour(s), ' + minutes + ' minute(s), ' + seconds + ' second(s).')
